9ba5ebaffa4c37ee86671ef2bc535a8659ffe2ab
Sebastian <gamseb@gamseb.pc>
2018-10-01 01:29:17 +0200

    I have cleaned up the code and changed the library to subprocess instead of os

--- download_program_mp3.py
+++ download_program_mp3.py
@@ -1,15 +1,17 @@
 """A simple program to download youtube videos using youtube-dl"""
-import os
+import subprocess
 
-print("Hello, This is a simple youtube downloader")
-os.system("cd '/home/gamseb/Music/downloads_from_script'")
+print("This is a simple youtube downloader")
+
 while True:
     print("*" * 35)
     print("input your url or type \"end\" to exit")
-    x = input(" ")
-    if x.lower() == "end":
+    command = input(" ")
+    if command.lower() == "end":
         break
     else:
-        os.system("youtube-dl --extract-audio --audio-format mp3 -o \"%(title)s.%(ext)s\" " + x)
+        subprocess.call("youtube-dl --extract-audio --audio-format mp3 -o \"%(title)s.%(ext)s\" " + command, shell=True)
         print("your video has been downloaded")
+
+
 
